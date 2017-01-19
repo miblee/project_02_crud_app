c0fb9d73f4776b5a4cadbc19c1c5adc379a56fcc
Michelle Lee <lee.michellejung@gmail.com>
2017-01-19 11:29:51 -0800

    HALLELUJAH contenteditable is set on correct elements.

--- public/main.js
+++ public/main.js
@@ -5,3 +5,19 @@ $('#submit').click(function(evt){
     console.log(res)
   });
 })
+
+
+$('.delete-button').click(function(evt){
+  $.post('/delete', function(res){
+    console.log(res);
+  });
+})
+
+
+$('.edit-button').click(function(evt){
+  $(this).attr('class',"barImgs save-button");
+  $(this).attr('src', 'floppy.png');
+  $(this).closest('.quotebox').find('span').attr("contenteditable", "true")
+  $(this).closest('.quotebox').find('.quoteText').attr("contenteditable", "true")
+})
+
